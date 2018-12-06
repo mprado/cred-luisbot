74c96870ba894762e38f8f66968a7f40c9cdcfd0
Marcelo Prado <marcelo_prado@outlook.com>
2018-12-06 14:58:32 -0200

    .

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=cred-bot-sample-Web-Deploy.pubxml -p:Password=9nDqeq01wnBccc2stlbkAWrAwcrnyWNnXGTsBqhaXGn3f3DSZt0Zxrav4jmu
+
