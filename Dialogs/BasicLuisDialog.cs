07eaa31695244f05c48591bab25031f95459c9de
Marcelo Prado <marcelo_prado@outlook.com>
2018-12-06 15:54:00 -0200

    .

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.Bot.Builder.Dialogs;
@@ -19,20 +20,32 @@ namespace Microsoft.Bot.Sample.LuisBot
         {
         }
 
-        [LuisIntent("None")]
-        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        /// <summary>
+        /// Send a generic help message if an intent without an intent handler is detected.
+        /// </summary>
+        /// <param name="context">Dialog context.</param>
+        /// <param name="result">The result from LUIS.</param>
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
         {
-            await this.ShowLuisResult(context, result);
+
+            string message = $"I'm the Notes bot. I can understand requests to create, delete, and read notes. \n\n Detected intent: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
         }
+
+        //[LuisIntent("None")]
+        //public async Task NoneIntent(IDialogContext context, LuisResult result)
+        //{
+        //    await this.ShowLuisResult(context, result);
+        //}
 
         // Go to https://luis.ai and create a new intent, then train/publish your luis app.
         // Finally replace "Greeting" with the name of your newly created intent in the following handler
         [LuisIntent("Greeting")]
         public async Task GreetingIntent(IDialogContext context, LuisResult result)
         {
-            //await this.ShowLuisResult(context, result);
-
-            await context.PostAsync($"Hi! How are you?");
+            await this.ShowLuisResult(context, result);
+            //await context.PostAsync($"Olá! Qual seu nome?");
         }
 
         [LuisIntent("Cancel")]
